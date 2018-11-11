8e43087efe180e3eb0208ec09f43ccdb5b0c3b97
aleksi <aleksi.hamalainen@aalto.fi>
2018-11-11 14:57:57 +0200

    opencv image show included to main.cpp

--- src/include/image_reader/image_reader.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Aleksi Hämäläinen on 07/11/2018.
-//
-
-#include "image_reader.h"
--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "vector/vec3.h"
 #include "ray.h"
+#include <opencv2/highgui/highgui.hpp>
 
 int main() {
 
     int nx = 400;
     int ny = 200;
-    //printing as ppm file
+
+    uchar image[nx][ny][3];
+
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny-1; j >= 0; j--)
     {
@@ -15,12 +18,22 @@ int main() {
             float r = float(i) / float(nx);
             float g = float(j) / float(ny);
             float b = 0.2;
-            int ir = int(255.99*r);
-            int ig = int(255.99*g);
-            int ib = int(255.99*b);
-            std::cout << ir << " " << ig << " " << ib << "\n";
+
+            auto ir = uchar(255.99*r);
+            auto ig = uchar(255.99*g);
+            auto ib = uchar(255.99*b);
+
+            // BRG format
+            image[i][j][0] = ib;
+            image[i][j][1] = ir;
+            image[i][j][2] = ig;
+
         }
     }
+
+    cv::Mat rgb_mat(nx, ny, CV_8UC3, &image);
+    cv::imshow("Image", rgb_mat);
+    cv::waitKey(0);
 
     return 0;
 }
