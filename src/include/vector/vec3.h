7eb18089edb2fbb73f48cba71ef07f91466a0eb4
Jesse Miettinen <jesse@Jesses-MacBook-Pro-2.local>
2018-11-12 17:15:00 +0200

    Corrected vec3's function cross()->dot(). Also added geometry.hpp which contains undebugged versions of Geometry parent-class and Sphere child class. Started to build tests for these classes inorder to get into debugging.

--- src/include/vector/vec3.h
+++ src/include/vector/vec3.h
@@ -236,10 +236,16 @@ vec3<T> operator/(const T &a, const vec3<T> &v1) {
     return v1 / a;
 };
 
-// Cross product
+// Dot Product
 template <typename T>
-T cross(const vec3<T> &v1, const vec3<T> &v2) {
+T dot(const vec3<T> &v1, const vec3<T> &v2) {
     return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+};
+
+//Cross Product
+template <typename T>
+vec3<T> dot(const vec3<T> &v1, const vec3<T> &v2){
+  return vec3<T>(v1[1]*v2[2]-v1[2]*v2[1], v1[2]*v2[0]-v1[0]*v2[2], v1[0]*v2[1]-v1[1]*v2[0]);
 };
 
 // Norm
@@ -261,4 +267,3 @@ void vec3<T>::turn_unit() {
 };
 
 #endif //PATH_TRACER_VEC3_H
-
