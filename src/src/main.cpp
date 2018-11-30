d4ae4f67a71769cbbbcb02edc30a0c213ad6cf1b
leppalv1 <ville.leppala@aalto.fi>
2018-11-30 10:36:24 +0200

    creating random spheres to scene

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -6,31 +6,24 @@
 #include "geometry/geometry.h"
 #include "camera/camera.h"
 
-int main() {
-
-    int nx = 800;
-    int ny = 400;
-    int ns = 100;//determines how many rays are sent through a pixel for antialiasing
-
+int RandomScene(unsigned int amount, int nx, int ny, unsigned int ns)
+{
     uchar image[ny][nx][3];
+    Geometry *li[amount];
+    li[0] = new Sphere(vec3<float>(0,-105,-1),100);
+    for(unsigned int i = 1; i < amount; i++) {
 
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+        li[i] = new Sphere(vec3<float>(20 * float(drand48())-5, -5 * float(drand48()), -10 * float(drand48())), 2 * float(drand48()));
+    }
 
-    Geometry *li[3];
-    li[0] = new Sphere(vec3<float>(0,0,-1), 0.5);
-    li[1] = new Sphere(vec3<float>(0,-100.5,-1),100);
-    li[2] = new Sphere(vec3<float>(0,-0.5,-1),0.5);
+    Geometry *world = new Geomlist(li, amount);
+    //first three camera parameters are vectors: look_from, look_at, view_up
+    Camera cam(vec3<float>(0, -3, 0), vec3<float>(7, -5, -5), vec3<float>(0, 1, 0), 90, float(nx) / float(ny));
 
-    Geometry * world = new Geomlist(li,3);
-    //Camera cam(90, float(nx)/float(ny));
-    Camera cam(vec3<float>(-2,2,1), vec3<float>(0,0,-1), vec3<float>(0,1,0), 90, float(nx)/float(ny));
-    for (int j = ny-1; j >= 0; j--)
-    {
-        for (int i = 0; i < nx; i++)
-        {
+    for (int j = ny - 1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
             vec3<float> col(0, 0, 0);
-            for(int s=0; s < ns; s++)
-            {
+            for (int s = 0; s < ns; s++) {
                 float u = 1 - float(i + drand48()) / float(nx);
                 float v = 1 - float(j + drand48()) / float(ny);
                 ray<float> r = cam.GetRay(u, v);
@@ -38,9 +31,9 @@ int main() {
             }
             //averages the colors of rays going through a pixel
             col /= float(ns);
-            auto ir = uchar(255.99*col[0]);
-            auto ig = uchar(255.99*col[1]);
-            auto ib = uchar(255.99*col[2]);
+            auto ir = uchar(255.99 * col[0]);
+            auto ig = uchar(255.99 * col[1]);
+            auto ib = uchar(255.99 * col[2]);
 
             // BGR format
             image[j][i][0] = ib;
@@ -53,6 +46,21 @@ int main() {
     cv::Mat rgb_mat(ny, nx, CV_8UC3, &image);
     cv::imshow("Image", rgb_mat);
     cv::waitKey(0);
+
+    return 0;
+}
+
+
+int main() {
+
+    int nx = 800;
+    int ny = 400;
+    unsigned int ns = 10;//determines how many rays are sent through a pixel for antialiasing
+    unsigned int amount = 15; // determines how many objects do we create
+
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+
+    RandomScene(amount, nx, ny, ns);
 
     return 0;
 }
