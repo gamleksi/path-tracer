be1536c48f490daac46c48f19977c612b6c65597
aleksi <aleksi.hamalainen@aalto.fi>
2018-12-02 14:48:40 +0200

    shared pointers introduced

--- src/include/camera/camera.h
+++ src/include/camera/camera.h
@@ -10,14 +10,16 @@
 
 class Camera {
 public:
+
     /*look_from is a point where we are looking from
      *look_at is a point where we are looking at
      * view_up is a vector up from the point we are looking, determines rotation of the camera on the axis from camera to the point we are looking into
      */
-    Camera(vec3<float> look_from, vec3<float> look_at, vec3<float> view_up, float vfov, float aspect);
-    ray<float> GetRay(float u, float v)
+
+    Camera(const vec3<float>& look_from, const vec3<float>& look_at, const vec3<float>& view_up, float vfov, float aspect);
+    ray<float> GetRay(float u, float v) const
     {
-        return ray<float>(origin_, lower_left_corner_ + u*horizontal_ + v*vertical_ - origin_);
+        return ray<float>(origin_, lower_left_corner_ + u * horizontal_ + v * vertical_ - origin_);
     }
 private:
     vec3<float> origin_;
@@ -26,7 +28,7 @@ private:
     vec3<float> vertical_;
 };
 
-vec3<float> Color(const ray<float>& r, Geometry *s, int depth);
+vec3<float> Color(const ray<float>& r, const std::shared_ptr<Geometry>& geom, int depth);
 
 
 #endif //PATH_TRACER_CAMERA_H
--- src/include/geometry/geometry.h
+++ src/include/geometry/geometry.h
@@ -5,6 +5,7 @@
 #ifndef PATH_TRACER_GEOMETRY_H
 #define PATH_TRACER_GEOMETRY_H
 
+#include <vector>
 #include "vector/vec3.h"
 #include "ray/ray.h"
 
@@ -14,7 +15,7 @@ struct Hit_record{
   float time;
   vec3<float> point;
   vec3<float> normal;
-  Material* mat_ptr;
+  std::shared_ptr<Material> mat_ptr;
 };
 
 
@@ -23,12 +24,12 @@ class Geometry {
  virtual bool RayHits(const ray<float>& r, float t_min, float t_max, Hit_record& rec) const = 0;
 };
 
+
 class Sphere : public Geometry{
   //RayHits algorithm by Peter Shirley, from Ray Tracing in One Weekend, version 1.55
  public:
 
-
-  Sphere(vec3<float> position, float radius, Material* mat)
+  Sphere(vec3<float> position, float radius, std::shared_ptr<Material> mat)
   : Geometry(), radius_(radius), position_(position), material_(mat) { }
   //get radius and ray hits Sphere
   ~Sphere() { };
@@ -43,16 +44,21 @@ class Sphere : public Geometry{
  private:
   //radius here
   float radius_;
-  Material* material_;
+  std::shared_ptr<Material> material_;
   vec3<float> position_;
 };
 
 class Geomlist : public Geometry{
  public:
+
   Geomlist(){}
-  Geomlist(Geometry **g, int n){list_ = g; list_size_ = n;}
-  virtual bool RayHits(const ray<float>& r, float t_min, float t_max, Hit_record& rec) const;
-  Geometry **list_;
+
+  Geomlist(const int number_of_objects, std::vector<std::shared_ptr<Geometry>>& object_list)
+  : list_size_(number_of_objects), list_(object_list) {}
+
+ virtual bool RayHits(const ray<float>& r, float t_min, float t_max, Hit_record& rec) const;
+ private:
+  std::vector<std::shared_ptr<Geometry>> list_;
   int list_size_;
 
 };
