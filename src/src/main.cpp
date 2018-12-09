90a08565640d227ff5cf6e3afb4ed46ae2b207c0
leppalv1 <ville.leppala@aalto.fi>
2018-12-09 18:56:20 +0200

    fixed comments

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -10,7 +10,7 @@
 typedef std::chrono::high_resolution_clock Clock;
 #include <omp.h>
 
-int GetRandomObjectList(unsigned int amount, std::vector<std::shared_ptr<Geometry>>& li) {
+void GetRandomObjectList(unsigned int amount, std::vector<std::shared_ptr<Geometry>>& object_list) {
 
   int num = int(sqrt(amount)/2);
 
@@ -20,8 +20,10 @@ int GetRandomObjectList(unsigned int amount, std::vector<std::shared_ptr<Geometr
 
   std::shared_ptr<Sphere> floor_sphere =
       std::make_shared<Sphere>(vec3<float>(0, -1000, 0), 1000, material);
-  li.push_back(floor_sphere);
 
+  object_list.push_back(floor_sphere);
+
+  std::vector<std::shared_ptr<Geometry>> li;
   //std::shared_ptr<XyRect> test_rect = std::make_shared<XyRect>(3.0, 5.0, 1.0, 3.0, -1.0, material);
   //li.push_back(test_rect);
 
@@ -71,9 +73,12 @@ int GetRandomObjectList(unsigned int amount, std::vector<std::shared_ptr<Geometr
   li.push_back(std::make_shared<Sphere>(vec3<float>(-4,1,0), 1.0, checker_material));
   //li.push_back(std::make_shared<Sphere>(vec3<float>(-4,1,0), 1.0, std::make_shared<Lambertian>(vec3<float>(0.4,0.2,0.1))));
   i++;
-  return i;
+  auto bb_world = std::make_shared<BoundingVolumeNode>(BoundingVolumeNode(li, 0.0, 1.0));
+  object_list.push_back(bb_world);
+
 }
 
+//can be modified, does not work with current camera settings
 void GetDebugObjectList(unsigned int amount, std::vector<std::shared_ptr<Geometry>>& object_list) {
 
   float y_step = 9;
@@ -170,8 +175,8 @@ int main() {
     int nx = 1200;
     int ny = 600;
 
-    unsigned int antialias_samples = 2;
-    unsigned int number_of_objects = 10;
+    unsigned int antialias_samples = 20;
+    unsigned int number_of_objects = 500;
 
 /**
  * In order to get everything out of your computer the number of threads should be dividable by 8 and nx * ny % num_threads == 0
@@ -184,18 +189,17 @@ int main() {
   omp_set_num_threads(num_threads);
 
   // Create Camera
-  //    Camera camera(vec3<float>(13, 2, 3), vec3<float>(0, 0, 0), vec3<float>(0, 1, 0), 20, float(nx) / float(ny));
-  vec3<float> look_from(0, 0, 0);
-  vec3<float> look_at(0, 0, -1);
-  float dist_to_focus = (look_from - look_at).Norm2();
+  vec3<float> look_from(13, 2, 3);
+  vec3<float> look_at(0, 0, 0);
+  float dist_to_focus = 1.0;//(look_from - look_at).Norm2();
   float aperture = 0.0;
-  float fov = 90;
+  float fov = 20;
   float aspect = float(nx) / float(ny);
 
   Camera camera(look_from, look_at, vec3<float>(0, 1, 0), fov, aspect, aperture, dist_to_focus);
 
   std::vector<std::shared_ptr<Geometry>> object_list;
-  GetDebugObjectList(number_of_objects, object_list);
+  GetRandomObjectList(number_of_objects, object_list);
 
   auto world = std::make_shared<Geomlist>(object_list);
 
@@ -209,7 +213,7 @@ int main() {
   auto bb_rendering_duration = std::chrono::duration_cast<std::chrono::seconds>(t1 - t0).count();
   std::cout << "Bounding Box Rendering Duration: "
           << bb_rendering_duration
-          << " secondds" << std::endl;
+          << " seconds" << std::endl;
 
   SaveImage(nx, ny, bb_image, "../awd_image.jpg"); // TODO: fix path
 
