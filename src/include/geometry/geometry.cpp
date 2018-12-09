e81bfeacddc480def68d152f8cc7ce02115f9639
leppalv1 <ville.leppala@aalto.fi>
2018-12-09 22:34:14 +0200

    xzrect, yzrect, flippednormals

--- src/include/geometry/geometry.cpp
+++ src/include/geometry/geometry.cpp
@@ -12,6 +12,11 @@ Sphere::Sphere(vec3<float> position, float radius, std::shared_ptr<Material> mat
 XyRect::XyRect(float x0, float x1, float y0, float y1, float k, std::shared_ptr<Material> mat)
     : x0_(x0), x1_(x1), y0_(y0), y1_(y1), k_(k), material_(std::move(mat)) { }
 
+XzRect::XzRect(float x0, float x1, float z0, float z1, float k, std::shared_ptr<Material> mat)
+    : x0_(x0), x1_(x1), z0_(z0), z1_(z1), k_(k), material_(std::move(mat)) { }
+
+YzRect::YzRect(float y0, float y1, float z0, float z1, float k, std::shared_ptr<Material> mat)
+    : y0_(y0), y1_(y1), z0_(z0), z1_(z1), k_(k), material_(std::move(mat)) { }
 
 bool Sphere::RayHits(const ray<float>& r, float t_min, float t_max, Hit_record& rec) const
 {
@@ -55,6 +60,34 @@ bool XyRect::RayHits(const ray<float> &r, float t_min, float t_max, Hit_record &
     return true;
 }
 
+bool XzRect::RayHits(const ray<float> &r, float t_min, float t_max, Hit_record &rec) const {
+    float t = (k_ - r.Origin().Y()) / r.Direction().Y();
+    if (t < t_min || t > t_max) { return false; }
+    float x = r.Origin().X() + t * r.Direction().X();
+    float z = r.Origin().Z() + t * r.Direction().Z();
+    if (x < x0_ || x > x1_ || z < z0_ || z > z1_) { return false; }
+    rec.u = (x - x0_) / (x1_ - x0_);
+    rec.v = (z - z0_) / (z1_ - z0_);
+    rec.mat_ptr = material_;
+    rec.point = r.Point(rec.time);
+    rec.normal = vec3<float>(0, 1, 0);
+    return true;
+}
+
+bool YzRect::RayHits(const ray<float> &r, float t_min, float t_max, Hit_record &rec) const {
+    float t = (k_ - r.Origin().X()) / r.Direction().X();
+    if (t < t_min || t > t_max) { return false; }
+    float y = r.Origin().Y() + t * r.Direction().Y();
+    float z = r.Origin().Z() + t * r.Direction().Z();
+    if (y < y0_ || y > y1_ || z < z0_ || z > z1_) { return false; }
+    rec.u = (y - y0_) / (y1_ - y0_);
+    rec.v = (z - z0_) / (z1_ - z0_);
+    rec.mat_ptr = material_;
+    rec.point = r.Point(rec.time);
+    rec.normal = vec3<float>(1, 0, 0);
+    return true;
+}
+
 bool Sphere::GetBoundingBox(float t0, float t1, BoundingBox& box) const {
     box = BoundingBox(position_ - radius_, position_ + radius_);
     return true;
@@ -63,6 +96,31 @@ bool Sphere::GetBoundingBox(float t0, float t1, BoundingBox& box) const {
 bool XyRect::GetBoundingBox(float t0, float t1, BoundingBox &box) const {
     box = BoundingBox(vec3<float>(x0_,y0_,k_-0001), vec3<float>(x1_,y1_,k_+0001));
     return true;
+}
+
+bool XzRect::GetBoundingBox(float t0, float t1, BoundingBox &box) const {
+    box = BoundingBox(vec3<float>(x0_,k_-0.0001,z0_), vec3<float>(x1_,k_+0.0001,z1_));
+    return true;
+}
+
+bool YzRect::GetBoundingBox(float t0, float t1, BoundingBox &box) const {
+    box = BoundingBox(vec3<float>(k_-0.0001,y0_,z0_), vec3<float>(k_+0.0001,y1_,z1_));
+    return true;
+}
+
+Box::Box(const vec3<float> &p0, const vec3<float> &p1, std::shared_ptr<Material> mat) {
+    pmin_ = p0;
+    pmax_ = p1;
+
+    std::vector<std::shared_ptr<Geometry>> rect_list(6);
+
+    rect_list[0] = std::make_shared<XyRect>(p0.X(), p1.X(), p0.Y(), p1.Y(), p1.Z(), mat);
+    rect_list[1] = std::make_shared<FlipNormals>(std::make_shared<XyRect>(p0.X(), p1.X(), p0.Y(), p1.Y(), p0.Z(), mat));
+    rect_list[2] = std::make_shared<XzRect>(p0.X(), p1.X(), p0.Z(), p1.Z(), p1.Y(), mat);
+    rect_list[3] = std::make_shared<FlipNormals>(std::make_shared<XzRect>(p0.X(), p1.X(), p0.Z(), p1.Z(), p0.Y(), mat));
+    rect_list[4] = std::make_shared<XzRect>(p0.Y(), p1.Y(), p0.Z(), p1.Z(), p1.X(), mat);
+    rect_list[5] = std::make_shared<FlipNormals>(std::make_shared<XzRect>(p0.Y(), p1.Y(), p0.Z(), p1.Z(), p0.X(), mat));
+    list_ptr_ = std::make_shared<Geomlist>(rect_list, 6);
 }
 
 bool Geomlist::RayHits(const ray<float>& r, float t_min, float t_max, Hit_record& rec) const{
@@ -188,7 +246,15 @@ BoundingVolumeNode::BoundingVolumeNode(std::vector<std::shared_ptr<Geometry>>& o
 
  int XyRect::NumberOfObjects() const {
     return 1;
-}
+ }
+
+ int XzRect::NumberOfObjects() const {
+    return 1;
+ }
+
+ int YzRect::NumberOfObjects() const {
+    return 1;
+ }
 
  int Geomlist::NumberOfObjects() const {
    int i = 0;
--- src/include/geometry/geometry.h
+++ src/include/geometry/geometry.h
@@ -70,6 +70,52 @@ private:
     std::shared_ptr<Material> material_;
 };
 
+class XzRect : public Geometry{
+public:
+    XzRect(float x0, float x1, float z0, float z1, float k, std::shared_ptr<Material> mat);
+
+    ~XzRect() { };
+    virtual bool RayHits(const ray<float>& r, float t_min, float t_max, Hit_record& rec) const;
+    virtual bool GetBoundingBox(float t0, float t1, BoundingBox& box) const;
+    virtual int NumberOfObjects() const;
+private:
+    float x0_, x1_, z0_, z1_, k_;
+    std::shared_ptr<Material> material_;
+};
+
+class YzRect : public Geometry{
+public:
+    YzRect(float y0, float y1, float z0, float z1, float k, std::shared_ptr<Material> mat);
+
+    ~YzRect() { };
+    virtual bool RayHits(const ray<float>& r, float t_min, float t_max, Hit_record& rec) const;
+    virtual bool GetBoundingBox(float t0, float t1, BoundingBox& box) const;
+    virtual int NumberOfObjects() const;
+private:
+    float y0_, y1_, z0_, z1_, k_;
+    std::shared_ptr<Material> material_;
+};
+
+class FlipNormals : public Geometry{
+public:
+    FlipNormals(std::shared_ptr<Geometry> p) : ptr_(p) { }
+    ~FlipNormals() { }
+    virtual bool RayHits(const ray<float>& r, float t_min, float t_max, Hit_record& rec) const {
+        if (ptr_->RayHits(r, t_min, t_max, rec)) {
+            rec.normal = -rec.normal;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    virtual bool GetBoundingBox(float t0, float t1, BoundingBox& box) const{
+        return ptr_->GetBoundingBox(t0, t1, box);
+    }
+    virtual int NumberOfObjects() const { return 1; }
+private:
+    std::shared_ptr<Geometry> ptr_;
+};
+
 class Geomlist : public Geometry{
  public:
 
@@ -89,6 +135,18 @@ class Geomlist : public Geometry{
 
 };
 
+class Box : public Geometry {
+public:
+    Box() { }
+    Box(const vec3<float>& p0, const vec3<float>& p1, std::shared_ptr<Material> mat);
+    ~Box() { };
+    virtual bool RayHits(const ray<float>& r, float t_min, float t_max, Hit_record& rec) const;
+    virtual bool GetBoundingBox(float t0, float t1, BoundingBox& box) const;
+    virtual int NumberOfObjects() const;
+private:
+    vec3<float> pmin_, pmax_;
+    std::shared_ptr<Geomlist> list_ptr_;
+};
 
 class BoundingVolumeNode : public Geometry {
 
